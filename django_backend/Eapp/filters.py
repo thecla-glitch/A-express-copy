2b1d6de99d68f2399b8970be7c19eaf34c4f2d2d
SvanteRomero <129661441+SvanteRomero@users.noreply.github.com>
2025-10-09 17:26:39 +0300

    ..

--- django_backend/Eapp/filters.py
+++ django_backend/Eapp/filters.py
@@ -4,13 +4,17 @@ from .models import Task
 class TaskFilter(django_filters.FilterSet):
     created_at = django_filters.DateFromToRangeFilter()
     updated_at = django_filters.DateFromToRangeFilter()
+    status = django_filters.CharFilter(method='filter_status')
 
     class Meta:
         model = Task
         fields = {
-            'status': ['exact', 'in'],
             'assigned_to': ['exact'],
             'customer': ['exact'],
             'created_by': ['exact'],
             'is_debt': ['exact'],
         }
+
+    def filter_status(self, queryset, name, value):
+        statuses = value.split(',')
+        return queryset.filter(status__in=statuses)
